00934979477ae55bba5b73b97ca372a90938c7b4
rakeshramesan <rakesh.ramesan@tmhighland.com>
2023-09-21 15:52:59 -0500

    initial commit

---
+++ src/test/java/com/ff/FlinkSink/FlinkSinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ff.FlinkSink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkSinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
